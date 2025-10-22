8f982dc40e94ffc7d072d0188ceec1aa2fce6d10
David Londono <davidlondono9@gmail.com>
2025-10-22 16:20:24 -0500

    fix: add chat localizations

--- apps/tina_app/lib/i18n/locale_keys.dart
+++ apps/tina_app/lib/i18n/locale_keys.dart
@@ -3,6 +3,17 @@
 // ignore_for_file: constant_identifier_names
 
 abstract class LocaleKeys {
+  static const menu_home = 'menu.home';
+  static const menu_chats = 'menu.chats';
+  static const menu_models = 'menu.models';
   static const models_screens_add_provider_open_button =
       'models_screens.add_provider.open_button';
+  static const chats_screens_chats_list_title =
+      'chats_screens.chats_list.title';
+  static const chats_screens_chats_list_add_chat =
+      'chats_screens.chats_list.add_chat';
+  static const chats_screens_chat_conversation_select_model_selctor =
+      'chats_screens.chat_conversation.select_model_selctor';
+  static const chats_screens_chat_conversation_message_placeholder =
+      'chats_screens.chat_conversation.message_placeholder';
 }
