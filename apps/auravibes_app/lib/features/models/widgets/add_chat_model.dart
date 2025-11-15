942ac989627c5061ae5f25082d9707fd78ae6328
David Londono <davidlondono9@gmail.com>
2025-11-15 08:19:50 -0500

    ignore mutation import

--- apps/auravibes_app/lib/features/models/widgets/add_chat_model.dart
+++ apps/auravibes_app/lib/features/models/widgets/add_chat_model.dart
@@ -7,6 +7,7 @@ import 'package:auravibes_ui/ui.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+// ignore: implementation_imports
 import 'package:riverpod/src/framework.dart';
 
 class AddModelProviderWidget extends HookConsumerWidget {
